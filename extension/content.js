3bc32b06b4041842689e6075a2ecce922f632c5f
JengCharat <zazajeng401@gmail.com>
2026-01-16 23:29:02 +0700

    contest.js
    this file is use for
    read edit add button dark theme of html  website

---
+++ extension/content.js
@@ -0,0 +1,8 @@
+// ไฟล์นี้ใชเสำหรับ
+// อ่าน / แก้ HTML ของเว็บ
+//     เปลี่ยนข้อความ
+// ใส่ปุ่มเพิ่มเข้าเว็บ
+// ทำ dark mode
+console.log("Content script loaded");
+
+document.body.style.backgroundColor = "#f0f0f0";
